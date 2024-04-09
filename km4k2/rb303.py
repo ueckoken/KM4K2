3c5d55d27d78899b022bcc7daff50acc42ddb657
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-04-09 21:47:07 +0900

    Update rb303.py

--- km4k2/rb303.py
+++ km4k2/rb303.py
@@ -3,7 +3,6 @@ import time
 import wiringpi
 
 wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(12, wiringpi.GPIO.PWM_OUTPUT)
 wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
 wiringpi.pwmSetRange(2000)
 wiringpi.pwmSetClock(192)
@@ -12,22 +11,27 @@ wiringpi.pwmSetClock(192)
 def servo(angle):
     angle = int(angle * 200 / 180 + 50)
     wiringpi.pwmWrite(12, angle)
-
+def servo_start():
+    wiringpi.pinMode(12, wiringpi.GPIO.PWM_OUTPUT)
+def servo_end():
+    wiringpi.pinMode(12, wiringpi.GPIO.INPUT)
 
 def unlock():
+    servo_start()
     servo(180)
-    time.sleep(1)
+    time.sleep(0.4)
     servo(90)
+    time.sleep(0.4)
+    servo_end()
 
 
 def lock():
+    servo_start()
     servo(0)
-    time.sleep(1)
+    time.sleep(0.4)
     servo(90)
-
-
-def reset():
-    servo(90)
+    time.sleep(0.4)
+    servo_end()
 
 
 if __name__ == "__main__":
